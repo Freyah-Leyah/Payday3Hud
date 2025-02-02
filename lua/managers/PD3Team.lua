e3349d2e5405b28996619573af2834f1da68eb06
Freya <69479853+Freyah-Leyah@users.noreply.github.com>
2025-02-02 16:14:59 +0100

    make it not crash on start

--- lua/managers/PD3Team.lua
+++ lua/managers/PD3Team.lua
@@ -1,22 +1,15 @@
-PD3Teammate = PD3Teammate or class()
+PD3Teammate = PD3Teammate or class(HUDTeammate)
 
-local VoidFuncs = {}
-local function Void(global, func)
-	for _, fun in pairs(func) do
-		if not VoidFuncs[fun] then
-			PD3Main:log(tostring(fun) .. " purged to Void")
-			VoidFuncs[fun] = true
-			global[fun] = function(...)
-				PD3Main:log(tostring(fun) .. " called from Void")
-				return nil
-			end
-		end
-	end
-end
+PD3Hijack({
+	global_to_hijack = HUDTeammate,  -- Target instance where functions will be hijacked
+	func_to_hijack = { "init", "_create_carry", "_create_radial_health", "set_health", "set_armor" }, -- Name of the functions to hijack
+	global = PD3Teammate  -- Instance where the redirect functions exist
+})
 
-function HUDTeammate:init(i, teammates_panel, is_player, width)
-	Void(self, { "init", "_create_carry", "_create_radial_health" })
-end
+-- function HUDTeammate:init(i, teammates_panel, is_player, width)
+-- 	-- Calling hijack function
+-- 	--PD3Teammate.init(self, i, teammates_panel, is_player, width)
+-- end
 
 function PD3Teammate:init(i, teammates_panel, is_player, width)
 	self._id = i
@@ -551,4 +544,82 @@ function PD3Teammate:_create_radial_health(radial_health_panel)
 	})
 
 	self:_create_condition(radial_health_panel)
+end
+
+function PD3Teammate:set_health(data)
+	self._health_data = data
+	local teammate_panel = self._panel:child("player")
+	local radial_health_panel = self._radial_health_panel
+	local radial_health = radial_health_panel:child("radial_health")
+	local radial_rip = radial_health_panel:child("radial_rip")
+	local radial_rip_bg = radial_health_panel:child("radial_rip_bg")
+	local red = data.current / data.total
+
+	if managers.player:has_activate_temporary_upgrade("temporary", "copr_ability") and self._id == HUDManager.PLAYER_PANEL then
+		local static_damage_ratio = managers.player:upgrade_value_nil("player", "copr_static_damage_ratio")
+
+		if static_damage_ratio then
+			red = math.floor((red + 0.01) / static_damage_ratio) * static_damage_ratio
+		end
+
+		local copr_overlay_panel = radial_health_panel:child("copr_overlay_panel")
+
+		if alive(copr_overlay_panel) then
+			for _, notch in ipairs(copr_overlay_panel:children()) do
+				notch:set_visible(notch:script().red <= red + 0.01)
+			end
+		end
+	end
+
+	radial_health:stop()
+
+	if red < radial_health:color().red then
+		self:_damage_taken()
+		radial_health:set_color(Color(1, red, 1, 1))
+
+		if alive(radial_rip) then
+			radial_rip:set_rotation((1 - radial_health:color().r) * 360)
+			radial_rip_bg:set_rotation((1 - radial_health:color().r) * 360)
+		end
+
+		self:update_delayed_damage()
+	else
+		radial_health:animate(function (o)
+			local s = radial_health:color().r
+			local e = red
+			local health_ratio = nil
+
+			over(0.2, function (p)
+				health_ratio = math.lerp(s, e, p)
+
+				radial_health:set_color(Color(1, health_ratio, 1, 1))
+
+				if alive(radial_rip) then
+					radial_rip:set_rotation((1 - radial_health:color().r) * 360)
+					radial_rip_bg:set_rotation((1 - radial_health:color().r) * 360)
+				end
+
+				self:update_delayed_damage()
+
+				local copr_overlay_panel = radial_health_panel:child("copr_overlay_panel")
+
+				if alive(copr_overlay_panel) then
+					for _, notch in ipairs(copr_overlay_panel:children()) do
+						notch:set_visible(notch:script().red <= health_ratio + 0.01)
+					end
+				end
+			end)
+		end)
+	end
+end
+
+function PD3Teammate:set_armor(data)
+	local teammate_panel = self._panel:child("player")
+	self._armor_data = data
+	local radial_health_panel = self._radial_health_panel
+	local radial_shield = radial_health_panel:child("radial_shield")
+	local ratio = data.total ~= 0 and data.current / data.total or 0
+
+	radial_shield:set_color(Color(1, ratio, 1, 1))
+	self:update_delayed_damage()
 end
