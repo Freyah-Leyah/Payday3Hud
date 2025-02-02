389ab7877deac90e435902b334382e9cfa9ec521
Freya <69479853+Freyah-Leyah@users.noreply.github.com>
2025-02-02 15:17:11 +0100

    add mod.txt, main.xml, hooks

--- lua/PD3Main.lua
+++ lua/PD3Main.lua
@@ -1,9 +1,12 @@
 _G.PD3Main = _G.PD3Main or class()
+PD3Main._path = ModPath
+PD3Main._data_path = SavePath .. "PD3Hud.json"
+PD3Main._data = {}
 
 local pdebug = true
 
 function PD3Main:log(...)
     if not pdebug then return end
-    local message = "[PD3 HUD] " .. ...
+    local message = "[PD3 HUD] " .. tostring(...)
     log(message)
 end
